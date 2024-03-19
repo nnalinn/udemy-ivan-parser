4377dc6d241110c8033d174856abdc373adf5698
nalin <alina.vysotskayaaa@gmail.com>
2024-03-19 21:14:36 +0200

    parser

---
+++ parser.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    let textNodes = [];
+
+    function recursy (element) {
+        element.childNodes.foreach(node => {
+
+            if(node.nodeName.match(/^H\d/)) {
+                const obj = {
+                    header: ondevicemotion.nodeName,
+                    content: node.textContent
+                };
+                textNodes.push(obj);
+            } else {
+                recursy(node);
+            }
+        });
+    }
+
+    recursy(body);
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(textNodes)
+    })
+
+    .then(response => response.json())
+    .then(json => console.log(json));
+});
